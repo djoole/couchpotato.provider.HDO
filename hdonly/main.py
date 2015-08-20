fce50831f87b6605829846687b7b7078c959b9f6
djoole <bobby.djoole@gmail.com>
2015-08-20 02:43:36 +0200

    Big changes (langdetect out, TMDB API in, fix for fr title encoding)

--- hdonly/main.py
+++ hdonly/main.py
@@ -1,4 +1,4 @@
-from langdetect import detect, detect_langs
+# coding: utf8
 from couchpotato.core.helpers.encoding import tryUrlencode
 from couchpotato.core.helpers.variable import tryInt
 from couchpotato.core.logger import CPLog
@@ -7,6 +7,7 @@ from couchpotato.core.media.movie.providers.base import MovieProvider
 import traceback
 import json
 from HTMLParser import HTMLParser
+import urllib2
 
 log = CPLog(__name__)
 
@@ -18,7 +19,7 @@ class hdonly(TorrentProvider, MovieProvider):
         'login' : baseurl + 'login.php',
         'login_check' : baseurl,
         'index' : baseurl + 'ajax.php?action=index',
-        'search' : baseurl + 'ajax.php?action=browse&searchstr=%s',
+        'search' : baseurl + 'ajax.php?action=browse&searchstr=%s&year=%s',
         'detail' : baseurl + 'ajax.php?action=torrent&id=%s',
         'download' : baseurl + 'torrents.php?action=download&id=%s&authkey=%s&torrent_pass=%s'
     }
@@ -35,47 +36,51 @@ class hdonly(TorrentProvider, MovieProvider):
         authkey = index['response']['authkey']
         passkey = index['response']['passkey']
 
-        titles = []
-        frtitle=''
-        titles.append(title.lower())
-        for t in movie['info']['titles']:
-            t = t.lower()
-            try:
-                if detect(t) == 'fr' and t not in titles:
-                    log.debug('found this FR title : %s' % t)
-                    titles.append(t)
-                    frtitle = t
-            except:
-                log.debug('')
-        for t in movie['info']['titles']:
-            t = t.lower()
-            try:
-                if detect(t) == 'en' and t not in titles:
-                    log.debug('found this EN title : %s' % t)
-                    titles.append(t)
-            except:
-                log.debug('')
-        log.debug('movie titles : %s' % titles)
+        if isinstance(title, str):
+            title = title.decode('utf8')
+        titles = [title]
+        movieYear = str(movie['info']['year'])
+        frTitle = self.getFrenchTitle(title, movieYear)
 
-        for t in titles:
-            log.debug('Searching HD-Only for %s' % t)
-            url = self.urls['search'] % t
-            data = self.getJsonData(url)
-            try:
-                if data['status'] == 'success':
-                    splittedTitle = t.lower().replace(':',' ').split()
-                    if not data['response']['results']:
-                        log.debug('No result from HD-Only with this title : %s' % t)
-                        continue
-                    for res in data['response']['results']:
-                        release_name = h.unescape(res['groupName']).lower()
-                        log.debug('release name check : %s' % release_name)
-                        if release_name in titles:
-                            log.debug('Got it : %s' % release_name)
-                            result = res
-                            break
-                    if 'the' not in release_name.lower() and (splittedTitle[-1] == 'the' or splittedTitle[0] == 'the'):
-                        release_name = 'the.' + release_name.lower().replace(' ','.')
+        log.debug('Searching HD-Only for ' + title)
+        request = urllib2.quote(title.encode('iso-8859-1'))
+        searchUrl = self.urls['search'] % (request, movieYear)
+        data = self.getJsonData(searchUrl)
+        #log.debug('******* data : ')
+        #log.debug(data)
+
+        if frTitle is not None:
+            titles = [frTitle, title]
+            #log.debug('******* titles : ')
+            #log.debug(titles)
+            log.debug('Searching HD-Only for ' + frTitle)
+            request = urllib2.quote(frTitle.encode('iso-8859-1'))
+            searchUrl = self.urls['search'] % (frTitle, movieYear)
+            frData = self.getJsonData(searchUrl)
+            #log.debug('******* frData : ')
+            #log.debug(frData)
+            totalData = combine(frData, data)
+            data = totalData
+            #log.debug('***************************')
+            #log.debug('******* totalData : ')
+            #log.debug(data)
+
+        try:
+            if data['status'] == 'success':
+                if not data['response']['results']:
+                    log.debug('No result from HD-Only with %s or %s' % (frTitle, title))
+                    return
+                result = ''
+                for res in data['response']['results']:
+                    release_name = h.unescape(res['groupName']).lower().replace(':','').replace('  ',' ')
+                    log.debug('release name reported by HD-Only : ' + release_name)
+                    if release_name in titles:
+                        log.debug('It\'s a match')
+                        result = res
+                        break
+                    else:
+                        log.debug('No match')
+                if result != '':
                     for torrent in result['torrents']:
                         id = torrent['torrentId']
                         name = release_name + '.' + str(result['groupYear']) + '.' + torrent['encoding'] + '.' + torrent['media'] + '.' + torrent['format']
@@ -93,16 +98,16 @@ class hdonly(TorrentProvider, MovieProvider):
                         if (seeders == 0):
                             log.debug('0 seeders, ignoring torrent %s' % id)
                             continue
-                        if (self.conf('vf') and not (('vf' in detail.replace('vfq','')) or ('multi' in detail.replace('vfq',''))) and t != frtitle):
+                        if (self.conf('vf') and not (('vf' in detail.replace('vfq','')) or ('multi' in detail.replace('vfq',''))) and title != frTitle):
                             log.debug('VF mandatory checked, ignoring torrent %s' % id)
                             continue
-                        if (self.conf('vfq') and ('vfq' not in detail) and t != frtitle):
+                        if (self.conf('vfq') and ('vfq' not in detail) and title != frTitle):
                             log.debug('VFQ mandatory checked, ignoring torrent %s' % id)
                             continue
-                        if (self.conf('vo') and not (('vo' in detail) or ('multi' in detail)) and t != frtitle):
+                        if (self.conf('vo') and (('vf' in detail) and not (('vo' in detail) or ('multi' in detail))) and title != frTitle):
                             log.debug('VO mandatory checked, ignoring torrent %s' % id)
                             continue
-                        if (self.conf('multi') and ('multi' not in detail) and t != frtitle):
+                        if (self.conf('multi') and ('multi' not in detail) and title != frTitle):
                             log.debug('MULTI mandatory checked, ignoring torrent %s' % id)
                             continue
                         if (self.conf('x265') and ('x265' not in detail)):
@@ -118,9 +123,9 @@ class hdonly(TorrentProvider, MovieProvider):
                         'seeders': seeders,
                         'leechers': leechers
                         })
-                return
-            except:
-                log.error('Failed to parse HD-Only: %s' % (traceback.format_exc()))
+            return
+        except:
+            log.error('Failed to parse HD-Only: %s' % (traceback.format_exc()))
 
 
     def getLoginParams(self):
@@ -139,3 +144,51 @@ class hdonly(TorrentProvider, MovieProvider):
 
 
     loginCheckSuccess = loginSuccess
+
+
+    def getFrenchTitle(self, title, year):
+        """
+        This function uses TMDB API to get the French movie title of the given title.
+        """
+
+        url = "https://api.themoviedb.org/3/search/movie?api_key=0f3094295d96461eb7a672626c54574d&language=fr&query=%s" % title
+        log.debug('Looking on TMDB for French title of : ' + title)
+        #data = self.getJsonData(url, decode_from = 'utf8')
+        data = self.getJsonData(url)
+        try:
+            if data['results'] != None:
+                for res in data['results']:
+                    yearI = res['release_date']
+                    if year in yearI:
+                        break
+                frTitle = res['title'].lower().replace(':','').replace('  ',' ').replace('-','')
+                if frTitle == title:
+                    log.debug('TMDB report identical FR and original title')
+                    return None
+                else:
+                    log.debug(u'L\'API TMDB a trouvé un titre français => ' + frTitle)
+                    return frTitle
+            else:
+                log.debug('TMDB could not find a movie corresponding to : ' + title)
+                return None
+        except:
+            log.error('Failed to parse TMDB API: %s' % (traceback.format_exc()))
+
+def combine(dict1, dict2):
+    combdict = {}
+    if (dict1['status'] == 'success') or (dict2['status'] == 'success'):
+        combdict[u'status'] = u'success'
+    else:
+        combdict[u'status'] = u'failure'
+    combdict[u'response'] = {}
+    combdict[u'response'][u'results'] = []
+    if 'response' in dict1:
+        if dict1['response']['results']:
+            combdict[u'response'][u'results'] = dict1['response']['results']
+    if 'response' in dict2:
+        if dict2['response']['results']:
+            combdict[u'response'][u'results'] = combdict[u'response'][u'results'] + dict2['response']['results']
+    #log.debug('combined dict is :')
+    #log.debug(combdict)
+    return combdict
+
